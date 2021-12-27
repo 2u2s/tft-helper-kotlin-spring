58a9b42e740bf95fb7a80b8f6a47baa732e90580
Woo <eee7407@naver.com>
2021-12-27 10:28:26 +0900

    Update: indexing, and code refactoring

--- src/main/kotlin/org/doubleus/tft_helper_kotlin_spring/constants/Champion.kt
+++ src/main/kotlin/org/doubleus/tft_helper_kotlin_spring/constants/Champion.kt
@@ -5,4 +5,14 @@ data class Champion(
     val traits: List<Trait>,
     val cost: Int,
     val items: List<Int>,
-)
+) {
+    constructor(id: String, traits: List<Trait>, cost: Int) : this(id, traits, cost, listOf())
+
+    fun with(items: List<Int>) = Champion(id, traits, cost, items)
+
+    fun with(item: Int) = with(listOf(item))
+
+    fun with(item1: Int, item2: Int) = with(listOf(item1, item2))
+
+    fun with(item1: Int, item2: Int, item3: Int) = with(listOf(item1, item2, item3))
+}
