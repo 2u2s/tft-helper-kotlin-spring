3bdab1a657f26d09fc5093d6764bb22658f44542
MacWoo <eee7407@naver.com>
2022-02-19 23:55:38 +0100

    Update: RiotController.

--- src/main/kotlin/org/doubleus/tft_helper_kotlin_spring/controller/RiotController.kt
+++ src/main/kotlin/org/doubleus/tft_helper_kotlin_spring/controller/RiotController.kt
@@ -1,7 +1,21 @@
 package org.doubleus.tft_helper_kotlin_spring.controller
 
+import org.doubleus.tft_helper_kotlin_spring.service.RiotService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RiotController {
+class RiotController(
+    private val riotService: RiotService
+) {
+    @GetMapping("", "/")
+    fun hello(): String {
+        return "hello"
+    }
+
+    @GetMapping("/deckInfo")
+    fun getDeckInfo() = riotService.getDeckInfo()
+
+    @GetMapping("/statistics")
+    fun getStatistics() = riotService.getStatistics()
 }
